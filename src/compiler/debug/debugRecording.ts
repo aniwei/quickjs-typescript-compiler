3568a69e79275150f0ba5bbfcae39fda937ff527
Aniwei <aniwei@SZMAC-MN9V4LCV.local>
2025-11-24 10:06:48 +0800

    chore

--- src/compiler/debug/debugRecording.ts
+++
@@ -1,7 +0,0 @@
-export interface DebugRecordingController {
-  withoutDebugRecording<T>(fn: () => T): T
-}
-
-export function runWithoutDebug<T>(controller: DebugRecordingController, fn: () => T): T {
-  return controller.withoutDebugRecording(fn)
-}
--- src/compiler/expressions/operators/index.ts
+++
@@ -1,4 +0,0 @@
-export { compileBinaryExpression } from './binary'
-export { compilePropertyAccessExpression } from './property'
-export { compilePrefixUnaryExpression, compilePostfixUnaryExpression } from './unary'
-export { compileVoidExpression } from './void'
