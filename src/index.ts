cdc33954a5ad57901522de11efff601b3dfc9323
Aniwei <aniwei.studio@gmail.com>
2025-12-06 13:07:18 +0800

    update

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './compiler/compiler'
-export * from './compiler/parseState'
-export * from './compiler/jsFunctionDef'
-export * from './bytecode/bytecodeWriter'
-export * from './bytecode/dynBuffer';
